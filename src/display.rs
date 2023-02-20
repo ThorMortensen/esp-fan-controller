7580139fa8159c1ddc15489e38c5acaed4c6dc3f
Thor Mortensen <tm@connectedcars.dk>
2023-02-20 02:38:33 +0100

    display with framed tex box done

--- src/display.rs
+++ src/display.rs
@@ -1,79 +1,92 @@
-use anyhow::Error;
-use display_interface::WriteOnlyDataCommand;
 use display_interface_parallel_gpio::Generic8BitBus;
 use display_interface_parallel_gpio::PGPIO8BitInterface;
+use embedded_graphics::geometry::AnchorPoint;
+use embedded_graphics::mock_display::ColorMapping;
 use embedded_graphics::mono_font::ascii::FONT_10X20;
+use embedded_graphics::mono_font::ascii::FONT_6X10;
 use embedded_graphics::mono_font::MonoTextStyle;
-use embedded_graphics::pixelcolor::BinaryColor;
-use embedded_graphics::pixelcolor::Rgb555;
 use embedded_graphics::pixelcolor::Rgb565;
 use embedded_graphics::prelude::Dimensions;
 use embedded_graphics::prelude::DrawTarget;
 use embedded_graphics::prelude::Point;
 use embedded_graphics::prelude::RgbColor;
 use embedded_graphics::prelude::Size;
-use embedded_graphics::primitives::Line;
 use embedded_graphics::primitives::Primitive;
+use embedded_graphics::primitives::PrimitiveStyle;
 use embedded_graphics::primitives::PrimitiveStyleBuilder;
 use embedded_graphics::primitives::Rectangle;
+use embedded_graphics::primitives::Styled;
+use embedded_graphics::text::Alignment;
 use embedded_graphics::text::Text;
 use embedded_graphics::Drawable;
-use embedded_graphics_sparklines::Sparkline;
-use embedded_hal::digital::v2::OutputPin;
+// use embedded_graphics::Drawable;
+use embedded_text::alignment::HorizontalAlignment;
+use embedded_text::plugin::NoPlugin;
+use embedded_text::style::HeightMode;
+use embedded_text::style::TextBoxStyle;
+use embedded_text::style::TextBoxStyleBuilder;
+use embedded_text::TextBox;
 use esp_idf_hal::delay;
 use esp_idf_hal::gpio;
 use esp_idf_hal::gpio::AnyOutputPin;
 use esp_idf_hal::gpio::Output;
 use esp_idf_hal::gpio::PinDriver;
-use mipidsi::models::Model;
+use log::info;
 use mipidsi::models::ST7789;
 use mipidsi::Display;
 
-// type Display_I8080_ST7789 = Display<
-//     PGPIO8BitInterface<
-//         Generic8BitBus<PinOut, PinOut, PinOut, PinOut, PinOut, PinOut, PinOut, PinOut>,
-//         PinOut,
-//         PinOut,
-//     >,
-//     ST7789,
-//     PinOut,
-// >;
+const LCD_WIDTH: u16 = 170; // width
+const LCD_HIGHT: u16 = 320; // height
 
-// #[derive(Debug)]
-pub struct TDisplayS3 {
-    display: Display<
-        PGPIO8BitInterface<
-            Generic8BitBus<
-                PinDriver<'static, AnyOutputPin, Output>,
-                PinDriver<'static, AnyOutputPin, Output>,
-                PinDriver<'static, AnyOutputPin, Output>,
-                PinDriver<'static, AnyOutputPin, Output>,
-                PinDriver<'static, AnyOutputPin, Output>,
-                PinDriver<'static, AnyOutputPin, Output>,
-                PinDriver<'static, AnyOutputPin, Output>,
-                PinDriver<'static, AnyOutputPin, Output>,
-            >,
+//Screen is horizontal flip dimensions ;-)
+pub const SCREEN_WIDTH: u32 = LCD_HIGHT as u32;
+pub const SCREEN_HIGHT: u32 = LCD_WIDTH as u32;
+pub const SCREEN_BOUNDS: Rectangle = Rectangle::new(
+    Point { x: 0, y: 0 },
+    Size {
+        width: SCREEN_WIDTH,
+        height: SCREEN_HIGHT,
+    },
+);
+
+type DisplayI8080St7789 = Display<
+    PGPIO8BitInterface<
+        Generic8BitBus<
+            PinDriver<'static, AnyOutputPin, Output>,
+            PinDriver<'static, AnyOutputPin, Output>,
+            PinDriver<'static, AnyOutputPin, Output>,
+            PinDriver<'static, AnyOutputPin, Output>,
+            PinDriver<'static, AnyOutputPin, Output>,
+            PinDriver<'static, AnyOutputPin, Output>,
             PinDriver<'static, AnyOutputPin, Output>,
             PinDriver<'static, AnyOutputPin, Output>,
         >,
-        ST7789,
+        PinDriver<'static, AnyOutputPin, Output>,
         PinDriver<'static, AnyOutputPin, Output>,
     >,
+    ST7789,
+    PinDriver<'static, AnyOutputPin, Output>,
+>;
+
+type ColType = Rgb565;
+#[allow(dead_code)]
+pub struct TDisplayS3 {
+    screen: DisplayI8080St7789,
     power: PinDriver<'static, AnyOutputPin, Output>,
     backlight: PinDriver<'static, AnyOutputPin, Output>,
     cs: PinDriver<'static, AnyOutputPin, Output>,
     rd: PinDriver<'static, AnyOutputPin, Output>,
 }
 
-pub struct OutputBusPins8Bit{
-   pub d0: gpio::AnyOutputPin,
-   pub d1: gpio::AnyOutputPin,
-   pub d2: gpio::AnyOutputPin,
-   pub d3: gpio::AnyOutputPin,
-   pub d4: gpio::AnyOutputPin,
-   pub d5: gpio::AnyOutputPin,
-   pub d6: gpio::AnyOutputPin,
-   pub d7: gpio::AnyOutputPin,
+pub struct OutputBusPins8Bit {
+    pub d0: gpio::AnyOutputPin,
+    pub d1: gpio::AnyOutputPin,
+    pub d2: gpio::AnyOutputPin,
+    pub d3: gpio::AnyOutputPin,
+    pub d4: gpio::AnyOutputPin,
+    pub d5: gpio::AnyOutputPin,
+    pub d6: gpio::AnyOutputPin,
+    pub d7: gpio::AnyOutputPin,
 }
 
 impl TDisplayS3 {
@@ -111,19 +124,13 @@ impl TDisplayS3 {
         rd.set_high().unwrap();
         cs.set_low().unwrap();
 
-        let mut display = mipidsi::Builder::st7789(di)
-            // .with_display_size(LCD_WIDTH, LCD_HIGHT)
-            .init(&mut delay::Ets, Some(gpio::PinDriver::output(rst).unwrap()))
-            .unwrap();
-
-
-        display
-            .set_orientation(mipidsi::Orientation::Landscape(true))
-            .unwrap();
-
-
         TDisplayS3 {
-            display,
+            screen: mipidsi::Builder::st7789(di)
+                .with_display_size(LCD_WIDTH, LCD_HIGHT)
+                .with_orientation(mipidsi::Orientation::Landscape(true))
+                .with_window_offset_handler(|_| -> (u16, u16) { (0, 35) })
+                .init(&mut delay::Ets, Some(gpio::PinDriver::output(rst).unwrap()))
+                .unwrap(),
             power: gpio::PinDriver::output(power).unwrap(),
             backlight: gpio::PinDriver::output(backlight).unwrap(),
             cs,
@@ -131,7 +138,164 @@ impl TDisplayS3 {
         }
     }
 
-    pub fn clear(&mut self, color:Rgb565) {
-        self.display.clear(color).unwrap();
+    pub fn clear(&mut self, color: ColType) {
+        self.screen.clear(color).unwrap();
+    }
+
+    // pub fn log(logMsg: &str){
+
+    // }
+}
+
+pub struct TDisplayS3Graphics<'a> {
+    display: &'a mut TDisplayS3,
+    // log: VecDeque<&'a str>,
+}
+
+impl<'a> TDisplayS3Graphics<'a> {
+    pub fn new(display: &'a mut TDisplayS3) -> Self {
+        TDisplayS3Graphics {
+            display, // log: VecDeque::new(),
+        }
+    }
+
+    pub fn txt<'b>(&mut self, txt: &'b str, txb: &'b mut FramedTextBox<'b>) {
+        txb.frame.draw(&mut self.display.screen).unwrap();
+        txb.text_box.text = txt;
+        txb.text_box.draw(&mut self.display.screen).unwrap();
+    }
+}
+
+pub struct FramedTextBox<'a> {
+    frame: Styled<Rectangle, PrimitiveStyle<Rgb565>>,
+    text_box: TextBox<'a, MonoTextStyle<'a, Rgb565>, NoPlugin<Rgb565>>,
+}
+
+pub enum FramedTextBoxAnchor {
+    Left,
+    Right,
+    Up,
+    Down,
+}
+
+pub struct FramedTextBoxBuilder {
+    frame: Rectangle,
+    frame_thickness: u32,
+    frame_color: ColType,
+    txt_color: ColType,
+    bg_color: ColType,
+    alignment: HorizontalAlignment,
+}
+
+impl FramedTextBoxBuilder {
+    pub fn new(frame: Rectangle) -> FramedTextBoxBuilder {
+        FramedTextBoxBuilder {
+            frame,
+            frame_thickness: 2,
+            frame_color: RgbColor::RED,
+            txt_color: RgbColor::WHITE,
+            bg_color: RgbColor::BLACK,
+            alignment: HorizontalAlignment::Left,
+        }
+    }
+    pub fn alignment(mut self, alignment: HorizontalAlignment) -> FramedTextBoxBuilder {
+        self.alignment = alignment;
+        self
+    }
+    pub fn frame_spacing(mut self, frame_spacing: u32) -> FramedTextBoxBuilder {
+        self.frame_thickness = frame_spacing;
+        self
+    }
+
+    pub fn frame_color(mut self, frame_color: ColType) -> FramedTextBoxBuilder {
+        self.frame_color = frame_color;
+        self
+    }
+
+    pub fn txt_color(mut self, txt_color: ColType) -> FramedTextBoxBuilder {
+        self.txt_color = txt_color;
+        self
+    }
+
+    pub fn bg_color(mut self, bg_color: ColType) -> FramedTextBoxBuilder {
+        self.bg_color = bg_color;
+        self
+    }
+
+    pub fn copy_relative_to(
+        from: &FramedTextBox,
+        anchor: FramedTextBoxAnchor,
+        spacing: i32,
+    ) -> FramedTextBoxBuilder {
+        FramedTextBoxBuilder::new_relative_to(from, anchor, spacing, from.frame.bounding_box().size)
+    }
+
+    pub fn new_relative_to(
+        from: &FramedTextBox,
+        anchor: FramedTextBoxAnchor,
+        spacing: i32,
+        size: Size,
+    ) -> FramedTextBoxBuilder {
+        let bb = from.frame.bounding_box();
+        let frame = match anchor {
+            FramedTextBoxAnchor::Left => Rectangle::new(
+                Point::new(bb.top_left.x - size.width as i32 - spacing, bb.top_left.y),
+                size,
+            ),
+            FramedTextBoxAnchor::Right => Rectangle::new(
+                Point::new(
+                    bb.top_left.x + bb.size.width as i32 + spacing,
+                    bb.top_left.y,
+                ),
+                size,
+            ),
+            FramedTextBoxAnchor::Up => Rectangle::new(
+                Point::new(bb.top_left.x, bb.top_left.y - spacing - size.height as i32),
+                size,
+            ),
+            FramedTextBoxAnchor::Down => Rectangle::new(
+                Point::new(
+                    bb.top_left.x,
+                    bb.top_left.y + spacing + bb.size.height as i32,
+                ),
+                size,
+            ),
+        };
+
+        FramedTextBoxBuilder::new(frame)
+    }
+
+    pub fn build<'a>(self) -> FramedTextBox<'a> {
+        let character_style: MonoTextStyle<ColType> =
+            MonoTextStyle::new(&FONT_10X20, self.txt_color);
+
+        let textbox_style = TextBoxStyleBuilder::new()
+            // .height_mode(HeightMode::FitToText)
+            .alignment(self.alignment)
+            .paragraph_spacing(6)
+            .build();
+
+        let frame_style = PrimitiveStyleBuilder::new()
+            .stroke_color(self.frame_color)
+            .stroke_width(1)
+            .fill_color(self.bg_color)
+            .build();
+
+        let text_field = Rectangle::new(
+            Point::new(
+                self.frame.bounding_box().top_left.x + self.frame_thickness as i32,
+                self.frame.bounding_box().top_left.y + self.frame_thickness as i32,
+            ),
+            Size::new(
+                self.frame.bounding_box().size.width - (self.frame_thickness * 2),
+                self.frame.bounding_box().size.height - (self.frame_thickness * 2),
+            ),
+        );
+        let text_box = TextBox::with_textbox_style("", text_field, character_style, textbox_style);
+
+        FramedTextBox {
+            frame: self.frame.into_styled(frame_style),
+            text_box,
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,26 @@
-use std::thread::sleep;
-use std::time::Duration;
-
-use anyhow::bail;
+use display::FramedTextBoxAnchor;
+use display::FramedTextBoxBuilder;
 use display::OutputBusPins8Bit;
-use display_interface_parallel_gpio::Generic8BitBus;
-use display_interface_parallel_gpio::PGPIO8BitInterface;
-use display_interface_spi::SPIInterfaceNoCS;
-use embedded_graphics::mono_font::ascii::FONT_10X20;
-use embedded_graphics::mono_font::MonoTextStyle;
-use embedded_graphics::pixelcolor::BinaryColor;
-use embedded_graphics::prelude::Dimensions;
-use embedded_graphics::prelude::DrawTarget;
+use display::TDisplayS3Graphics;
+use display::SCREEN_BOUNDS;
+use display::SCREEN_WIDTH;
+use embedded_graphics::pixelcolor::Rgb565;
 use embedded_graphics::prelude::Point;
 use embedded_graphics::prelude::RgbColor;
 use embedded_graphics::prelude::Size;
-use embedded_graphics::primitives::Line;
-use embedded_graphics::primitives::Primitive;
-use embedded_graphics::primitives::PrimitiveStyleBuilder;
 use embedded_graphics::primitives::Rectangle;
-use embedded_graphics::text::Text;
-use embedded_graphics::Drawable;
-use embedded_graphics_sparklines::Sparkline;
-use embedded_hal::digital::v2::OutputPin;
-use esp_idf_hal::delay;
+use embedded_text::alignment::HorizontalAlignment;
 use esp_idf_hal::gpio;
-use esp_idf_hal::gpio::AnyOutputPin;
-use esp_idf_hal::gpio::Output;
+
 use esp_idf_hal::prelude::*;
-use esp_idf_hal::spi;
-use esp_idf_svc::log;
-use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
+
+use esp_idf_sys as _;
 
 // use embedded_svc::mqtt::client::utils::ConnState;
 
-use ::log::info;
-use mipidsi::Display;
-use rand::Rng;
-use url;
+// use ::log::info;
+use rand::thread_rng;
+// use url;
 pub mod display;
 
 #[allow(dead_code)]
@@ -44,48 +28,13 @@ const SSID: &str = env!("RUST_ESP32_STD_DEMO_WIFI_SSID");
 #[allow(dead_code)]
 const PASS: &str = env!("RUST_ESP32_STD_DEMO_WIFI_PASS");
 
-const LCD_WIDTH: u16 = 170; // width
-const LCD_HIGHT: u16 = 320; // height
-
-// /*ESP32S3*/
-// #define PIN_LCD_BL                   38
-
-// #define PIN_LCD_D0                   39
-// #define PIN_LCD_D1                   40
-// #define PIN_LCD_D2                   41
-// #define PIN_LCD_D3                   42
-// #define PIN_LCD_D4                   45
-// #define PIN_LCD_D5                   46
-// #define PIN_LCD_D6                   47
-// #define PIN_LCD_D7                   48
-
-// #define PIN_POWER_ON                 15
-
-// #define PIN_LCD_RES                  5 *
-// #define PIN_LCD_CS                   6
-// #define PIN_LCD_DC                   7
-// #define PIN_LCD_WR                   8
-// #define PIN_LCD_RD                   9
-
-// #define PIN_BUTTON_1                 0
-// #define PIN_BUTTON_2                 14
-// #define PIN_BAT_VOLT                 4
-
-// #define PIN_IIC_SCL                  17
-// #define PIN_IIC_SDA                  18
-
-// #define PIN_TOUCH_INT                16
-// #define PIN_TOUCH_RES                21
-
-// type LcdDataBus =
-//     Generic8BitBus<LcdData0, LcdData1, LcdData2, LcdData3, LcdData4, LcdData5, LcdData6, LcdData7>;
-
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_sys::link_patches();
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
+    let mut rng = thread_rng();
 
     let peripherals = Peripherals::take().unwrap();
     let pins = peripherals.pins;
@@ -100,148 +49,65 @@ fn main() {
         pins.gpio9.into(), // #define PIN_LCD_RD 9
         pins.gpio7.into(), // #define PIN_LCD_DC 7
         pins.gpio8.into(), // #define PIN_LCD_WR 8
-        pins.gpio5.into(),  // #define PIN_LCD_RES 5
+        pins.gpio5.into(), // #define PIN_LCD_RES 5
         OutputBusPins8Bit {
-            d0: pins.gpio39.into(),// #define PIN_LCD_D0 39
-            d1: pins.gpio40.into(),// #define PIN_LCD_D1 40
-            d2: pins.gpio41.into(),// #define PIN_LCD_D2 41
-            d3: pins.gpio42.into(),// #define PIN_LCD_D3 42
-            d4: pins.gpio45.into(),// #define PIN_LCD_D4 45
-            d5: pins.gpio46.into(),// #define PIN_LCD_D5 46
-            d6: pins.gpio47.into(),// #define PIN_LCD_D6 47
-            d7: pins.gpio48.into(),// #define PIN_LCD_D7 48
+            d0: pins.gpio39.into(), // #define PIN_LCD_D0 39
+            d1: pins.gpio40.into(), // #define PIN_LCD_D1 40
+            d2: pins.gpio41.into(), // #define PIN_LCD_D2 41
+            d3: pins.gpio42.into(), // #define PIN_LCD_D3 42
+            d4: pins.gpio45.into(), // #define PIN_LCD_D4 45
+            d5: pins.gpio46.into(), // #define PIN_LCD_D5 46
+            d6: pins.gpio47.into(), // #define PIN_LCD_D6 47
+            d7: pins.gpio48.into(), // #define PIN_LCD_D7 48
         },
     );
+    display.clear(RgbColor::BLACK);
 
-    display.clear(RgbColor::YELLOW);
+    let mut screen = TDisplayS3Graphics::new(&mut display);
+    let mut log_box = FramedTextBoxBuilder::new(Rectangle::new(
+        Point::new(0, 0),
+        Size::new(200, display::SCREEN_HIGHT),
+    ))
+    .build();
 
+    let mut num_box = FramedTextBoxBuilder::new_relative_to(
+        &log_box,
+        FramedTextBoxAnchor::Right,
+        2,
+        Size::new(SCREEN_WIDTH - 200 - 2, (display::SCREEN_HIGHT / 5) - 2),
+    )
+    .alignment(HorizontalAlignment::Center)
+    .build();
 
-    // let bbox = Rectangle::new(Point::new(0, 26), Size::new(240, 90));
-    // let draw_fn = |lastp, p| Line::new(lastp, p);
+    let mut small_box =
+        FramedTextBoxBuilder::copy_relative_to(&num_box, FramedTextBoxAnchor::Down, 2)
+            .bg_color(RgbColor::MAGENTA)
+            .txt_color(RgbColor::BLACK)
+            .build();
 
-    // // create sparkline object
-    // let mut sparkline = Sparkline::new(
-    //     bbox, // position and size of the sparkline
-    //     32,   // max samples to store in memory (and display on graph)
-    //     RgbColor::RED,
-    //     1, // stroke size
-    //     draw_fn,
-    // );
+    let mut medium_box = FramedTextBoxBuilder::new_relative_to(
+        &small_box,
+        FramedTextBoxAnchor::Down,
+        2,
+        Size::new(SCREEN_WIDTH - 200 - 2, ((display::SCREEN_HIGHT / 5) * 3)),
+    )
+    .frame_color(RgbColor::GREEN)
+    .alignment(HorizontalAlignment::Center)
+    .build();
+
+    screen.txt(
+        "This is a multiline string\n--> This is line 2",
+        &mut log_box,
+    );
+    screen.txt("42", &mut num_box);
+    screen.txt("small2", &mut small_box);
+    screen.txt("Clock:\n12:42", &mut medium_box);
 
     // loop {
-    //   let val = rand::thread_rng().gen_range(0..100);
-    //   sparkline.add(val);
-    //   sparkline.draw(&mut display).unwrap();
-
+    // let r = rng.gen_range(1..120);
+    // let g = rng.gen_range(1..120);
+    // let b = rng.gen_range(1..120);
+    // display.clear(Rgb565::new(r, g, b).into());
+    // sleep(Duration::from_millis(1000));
     // }
-}
-
-#[allow(unused)]
-fn esp32s3_usb_otg_hello_world(
-    // backlight: gpio::Gpio9,
-    // dc: gpio::Gpio4,
-    // rst: gpio::Gpio8,
-    // spi: spi::SPI3,
-    // sclk: gpio::Gpio6,
-    // sdo: gpio::Gpio7,
-    // cs: gpio::Gpio5,
-    // bus: [gpio::Gpio39 gpio::Gpio40, gpio::Gpio41, gpio::Gpio42, gpio::Gpio45, gpio::Gpio46, gpio::Gpio47, gpio::Gpio48]
-    //
-    // #define PIN_LCD_RES                  5
-    // #define PIN_LCD_CS                   6
-    // #define PIN_LCD_DC                   7
-    // #define PIN_LCD_WR                   8
-    // #define PIN_LCD_RD                   9
-    backlight: gpio::Gpio38,
-    dc: gpio::Gpio7,
-    rst: gpio::Gpio5,
-    spi: spi::SPI3,
-    sclk: gpio::Gpio17,
-    sdo: gpio::Gpio8,
-    cs: gpio::Gpio6,
-    sdi: gpio::Gpio9,
-    bus: Generic8BitBus<
-        gpio::Gpio39,
-        gpio::Gpio40,
-        gpio::Gpio41,
-        gpio::Gpio42,
-        gpio::Gpio45,
-        gpio::Gpio46,
-        gpio::Gpio47,
-        gpio::Gpio48,
-    >,
-) -> Result<(), anyhow::Error> {
-    info!("About to initialize the ESP32-S3-USB-OTG SPI LED driver 8-Bit 8080");
-
-    let mut backlight = gpio::PinDriver::output(backlight)?;
-    backlight.set_high()?;
-
-    let di = SPIInterfaceNoCS::new(
-        spi::SpiDeviceDriver::new_single(
-            spi,
-            sclk,
-            sdo,
-            Some(sdi),
-            spi::Dma::Disabled,
-            Some(cs),
-            &spi::SpiConfig::new().baudrate(10.MHz().into()), //.data_mode(embedded_hal_01::spi::MODE_3),
-        )?,
-        gpio::PinDriver::output(dc)?,
-    );
-
-    let mut display = mipidsi::Builder::st7789(di)
-        .with_display_size(LCD_WIDTH, LCD_HIGHT)
-        .init(&mut delay::Ets, Some(gpio::PinDriver::output(rst)?))
-        .map_err(|e| anyhow::anyhow!("Display error: {:?}", e))?;
-
-    if let Err(e) = display
-        .set_pixel(20, 20, RgbColor::WHITE)
-        .map_err(|e| anyhow::anyhow!("Display error: {:?}", e))
-    {
-        dbg!(e);
-    }
-
-    dbg!(display.bounding_box());
-
-    display
-        .set_orientation(mipidsi::options::Orientation::Landscape(false))
-        .map_err(|e| anyhow::anyhow!("Display error: {:?}", e))?;
-
-    led_draw(&mut display).map_err(|e| anyhow::anyhow!("Led draw error: {:?}", e))
-}
-
-fn led_draw<D>(display: &mut D) -> Result<(), D::Error>
-where
-    D: DrawTarget + Dimensions,
-    D::Color: RgbColor,
-{
-    display.clear(RgbColor::WHITE)?;
-
-    Rectangle::new(display.bounding_box().top_left, display.bounding_box().size)
-        .into_styled(
-            PrimitiveStyleBuilder::new()
-                .fill_color(RgbColor::BLUE)
-                .stroke_color(RgbColor::BLACK)
-                .stroke_width(1)
-                .build(),
-        )
-        .draw(display)?;
-
-    Text::new(
-        "Hellasdafo Rust!",
-        Point::new(10, (display.bounding_box().size.height - 10) as i32 / 2),
-        MonoTextStyle::new(&FONT_10X20, RgbColor::WHITE),
-    )
-    .draw(display)?;
-
-    Text::new(
-        "This is the corner",
-        Point::new(0, 10),
-        MonoTextStyle::new(&FONT_10X20, RgbColor::WHITE),
-    )
-    .draw(display)?;
-
-    info!("LED rendering done");
-
-    Ok(())
 }
